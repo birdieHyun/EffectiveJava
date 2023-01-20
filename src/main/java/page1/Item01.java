b83d339acc012d89a216fbeae527f95d5f9426bb
birdieHyun <flaehdan@gmail.com>
2023-01-20 22:48:47 +0900

    feat : 아이템 1, 아이템 2

---
+++ src/main/java/page1/Item01.java
@@ -0,0 +1,45 @@
+package page1;
+
+public class Item01 {
+
+    private String order;
+    private boolean common;
+    private boolean urgent;
+
+    public Item01() {
+    }
+
+    public Item01(String order, boolean common) {
+        this.order = order;
+        this.common = common;
+    }
+
+//    public ItemOne(String order, boolean urgent) { // 생성자의 선언부가 같기 때문에 컴파일 에러가 난다.
+//        this.order = order;
+//        this.urgent = urgent;
+//    }
+
+    public Item01(boolean urgent, String order) { // 이렇게 매개변수의 순서를 바꿀 수도 있지만, 비효율적임
+        this.order = order;
+        this.urgent = urgent;
+    }
+
+    // 그래서 정적 팩터리 메서드를 고려해야 한다.
+    public Item01 makeUrgentOrder(String order, boolean urgent) {
+        Item01 itemOne = new Item01();
+
+        itemOne.order = order;
+        itemOne.urgent = urgent;
+
+        return itemOne;
+    }
+
+    public Item01 makeCommonOrder(String order, boolean urgent) {
+        Item01 itemOne = new Item01();
+        itemOne.order = order;
+        itemOne.common = common;
+
+        return itemOne;
+    }
+    // 메서드 명이 명확해진다.
+}
---
+++ src/main/java/page1/Item02.java
@@ -0,0 +1,95 @@
+package page1;
+
+public class Item02 {
+
+    //telescoping constructor pattern
+
+//    private final int servingSize;
+//    private final int servings;
+//    private final int calories;
+//    private final int fat;
+//
+//    // 사용자가 설정하기 원하지 않는 매개변수까지 포함하기 쉬운데, 이럴 땐 어쩔수 없이 포함시켜주어야 한다.
+//    public Item02(int servingSize, int servings) {
+//        this(servingSize, servings, 0);
+//    }
+//
+//    public Item02(int servingSize, int servings, int calories) {
+//        this(servingSize, servings, calories, 0);
+//    }
+//
+//    public Item02(int servingSize, int servings, int calories, int fat) {
+//        this.servingSize = servingSize;
+//        this.servings = servings;
+//        this.calories = calories;
+//        this.fat = fat;
+//    }
+//
+
+    // ------------------------------------------------
+
+    // 자바빈즈 패턴 - 일관성이 깨지고, 불변으로 만들 수 없다.
+//    private int servingSize = -1;
+//    private int servings = -1;
+//    private int calorie = 0;
+//    private int fat = 0;
+//
+//    public Item02() {}
+//
+//    public void setServingSize(int servingSize) {
+//        this.servingSize = servingSize;
+//    }
+//
+//    public void setServings(int servings) {
+//        this.servings = servings;
+//    }
+//
+//    public void setCalorie(int calorie) {
+//        this.calorie = calorie;
+//    }
+//
+//    public void setFat(int fat) {
+//        this.fat = fat;
+//    }
+
+
+    // ---------------------------------------
+    // 빌더패턴
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+
+    public static class Builder {
+        // 필수 매개변수
+        private final int servingSize;
+        private final int servings;
+
+        // 선택 매개변수 - 기본값으로 초기화한다.
+        private int calories = 0;
+        private int fat = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+        public Item02 build() {
+            return new Item02(this);
+        }
+    }
+        private Item02(Builder builder) {
+            servingSize = builder.servingSize;
+            servings = builder.servings;
+            calories = builder.calories;
+            fat = builder.fat;
+        }
+}
+

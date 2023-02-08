28a8f4bf525981e04a133f39d4c8e0a21e6d640b
birdieHyun <flaehdan@gmail.com>
2023-02-08 17:05:45 +0900

    .

---
+++ src/main/java/chapter8/Item52.java
@@ -0,0 +1,35 @@
+package chapter8;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Item52 {
+    public static String classify(Set<?> set) {
+        return "집합";
+    }
+
+    public static String classify(List<?> lst) {
+        return "리스트";
+    }
+
+    public static String classify(Collection<?> c) {
+        return "그 외";
+    }
+
+    public static String classify2(Collection<?> c) {
+        return c instanceof Set ? "집합" :
+                c instanceof List ? "리스트" : "그 외";
+    }
+    public static void main(String[] args) {
+        Collection<?>[] collections = {
+                new HashSet<String>(),
+                new ArrayList<BigInteger>(),
+                new HashMap<String, String>().values()
+        };
+
+        for (Collection<?> c : collections)
+            System.out.println(classify2(c));
+    }
+
+
+}
